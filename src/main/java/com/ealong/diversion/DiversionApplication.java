376496e6c205e663b847f8a4ecf5a4a52aec8c43
ealong <1170068185@qq.com>
2020-04-24 18:17:50 +0800

    feat #none: 添加描述

---
+++ src/main/java/com/ealong/diversion/DiversionApplication.java
@@ -0,0 +1,13 @@
+package com.ealong.diversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ealong/diversion/DiversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ealong.diversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
